524cf371e3d7f3e0154a63f3b680e20e10c32582
Nazar <synto11@gmail.com>
2026-03-23 23:30:58 +0100

    Add base Equipment class

---
+++ EquipmentRentalService/EquipmentRentalService/Models/Equipment.cs
@@ -0,0 +1,20 @@
+﻿namespace EquipmentRentalService.Models;
+
+public abstract class Equipment
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; } = true;
+    public bool IsMarkedUnavailable { get; set; } = false;
+
+    protected Equipment(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}: {Name} (Available: {IsAvailable})";
+    }
+}
