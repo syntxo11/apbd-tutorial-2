c55dd02f8d8b48229e32f05874a5ec4b168d978d
Nazar <synto11@gmail.com>
2026-03-23 23:31:34 +0100

    Add Laptop model

---
+++ EquipmentRentalService/EquipmentRentalService/Models/Laptop.cs
@@ -0,0 +1,14 @@
+﻿namespace EquipmentRentalService.Models;
+
+public class Laptop : Equipment
+{
+    public int RamGb { get; set; }
+    public string Cpu { get; set; }
+
+    public Laptop(int id, string name, int ramGb, string cpu)
+        : base(id, name)
+    {
+        RamGb = ramGb;
+        Cpu = cpu;
+    }
+}
